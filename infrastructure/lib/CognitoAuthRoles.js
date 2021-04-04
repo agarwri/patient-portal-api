c73ab28ca178c10eb8341aef6e129b876aca6309
agarwri <ritika.agarwal1995@gmail.com>
2021-04-04 18:56:16 -0400

    First commit

---
+++ infrastructure/lib/CognitoAuthRoles.js
@@ -0,0 +1,89 @@
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as cognito from "@aws-cdk/aws-cognito";
+
+export default class CognitoAuthRoles extends cdk.Construct {
+  // Public reference to the IAM role
+  role;
+  adminRole;
+
+  constructor(scope, id, props) {
+    super(scope, id);
+
+    const { identityPool, userPool, userPoolClient } = props;
+
+    // IAM role used for authenticated users
+    this.role = new iam.Role(this, "CognitoDefaultAuthenticatedRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+        {
+          StringEquals: {
+            "cognito-identity.amazonaws.com:aud": identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+    });
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "mobileanalytics:PutEvents",
+          "cognito-sync:*",
+          "cognito-identity:*",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    // IAM role used for authenticated users
+    this.adminRole = new iam.Role(this, "CognitoAdminAuthenticatedRole", {
+      assumedBy: new iam.FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+        {
+          StringEquals: {
+            "cognito-identity.amazonaws.com:aud": identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+    });
+    this.role.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "mobileanalytics:PutEvents",
+          "cognito-sync:*",
+          "cognito-identity:*",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    const providerUrl = "cognito-idp.us-east-1.amazonaws.com" + "/" + userPool.userPoolId + ":" + userPoolClient.userPoolClientId;
+
+    new cognito.CfnIdentityPoolRoleAttachment(
+      this,
+      "IdentityPoolRoleAttachment",
+      {
+        identityPoolId: identityPool.ref,
+        roles: {
+          authenticated: this.role.roleArn,
+        },
+        roleMappings: {
+          cognitoProvider: {
+            identityProvider: providerUrl,
+            type: "Token",
+            ambiguousRoleResolution: "Deny",
+          }
+        }
+      }
+    );
+  }
+}
---
+++ infrastructure/lib/CognitoStack.js
@@ -0,0 +1,135 @@
+import { CfnOutput } from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as path from 'path';
+import * as cognito from "@aws-cdk/aws-cognito";
+import * as sst from "@serverless-stack/resources";
+import CognitoAuthRoles from "./CognitoAuthRoles";
+
+export default class CognitoStack extends sst.Stack {
+  constructor(scope, id, props) {
+    super(scope, id, props);
+
+    const { bucketArn } = props;
+
+    const app = this.node.root;
+
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      selfSignUpEnabled: true, // Allow users to sign up
+      autoVerify: { email: true }, // Verify email addresses by sending a verification code
+      signInAliases: { email: true }, // Set email as an alias
+    });
+
+
+    const userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool,
+      generateSecret: false, // Don't need to generate secret for web app running on browsers
+    });
+
+    const identityPool = new cognito.CfnIdentityPool(this, "IdentityPool", {
+      allowUnauthenticatedIdentities: false, // Don't allow unathenticated users
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    const authenticatedRoles = new CognitoAuthRoles(this, "CognitoAuthRole", {
+      identityPool,
+      userPool,
+      userPoolClient,
+    });
+
+    authenticatedRoles.role.addToPolicy(
+      // IAM policy granting users permission to a specific folder in the S3 bucket
+      new iam.PolicyStatement({
+        actions: ["s3:*"],
+        effect: iam.Effect.ALLOW,
+        resources: [
+          bucketArn + "/private/${cognito-identity.amazonaws.com:sub}/*",
+        ],
+      })
+    );
+
+
+    authenticatedRoles.adminRole.addToPolicy(
+      // IAM policy granting admin users permission to all folders in the S3 bucket
+      new iam.PolicyStatement({
+        actions: ["s3:*"],
+        effect: iam.Effect.ALLOW,
+        resources: [
+          bucketArn + "/private/*/*",
+        ],
+      })
+    );
+
+    const defaultUserPoolGroup = new cognito.CfnUserPoolGroup(this, "DefaultUserPoolGroup", {
+      userPoolId: userPool.userPoolId,
+      description: "default user group",
+      groupName: "DefaultUsers",
+      precedence: 1,
+      roleArn: authenticatedRoles.role.roleArn,
+    });
+
+    const adminUserPoolGroup = new cognito.CfnUserPoolGroup(this, "AdminUserPoolGroup", {
+      userPoolId: userPool.userPoolId,
+      description: "admin user group",
+      groupName: "AdminUsers",
+      precedence: 0,
+      roleArn: authenticatedRoles.adminRole.roleArn,
+    });
+
+    const postConfirmationRole = new iam.Role(this, 'postConfirmationTriggerRole', {
+      roleName: 'postConfirmationTriggerRole',
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole")
+      ]
+    });
+
+    const postConfirmationFn = new lambda.Function(this, 'postConfirmationFn', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(`${path.resolve(__dirname)}/lambda`),
+      role: postConfirmationRole
+    });
+
+
+    postConfirmationFn.addToRolePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [ 'cognito-idp:*' ],
+      resources: [ '*' ]
+    }));
+
+
+    userPool.addTrigger(cognito.UserPoolOperation.POST_CONFIRMATION, postConfirmationFn);
+
+
+
+    // Export values
+    new CfnOutput(this, "UserPoolId", {
+      value: userPool.userPoolId,
+      exportName: app.logicalPrefixedName("UserPoolId"),
+    });
+    new CfnOutput(this, "UserPoolArn", {
+      value: userPool.userPoolArn,
+      exportName: app.logicalPrefixedName("UserPoolArn"),
+    });
+    new CfnOutput(this, "UserPoolClientId", {
+      value: userPoolClient.userPoolClientId,
+    });
+    new CfnOutput(this, "IdentityPoolId", {
+      value: identityPool.ref,
+    });
+    new CfnOutput(this, "AuthenticatedRoleName", {
+      value: authenticatedRoles.role.roleName,
+      exportName: app.logicalPrefixedName("CognitoAuthRole"),
+    });
+    new CfnOutput(this, "AdminAuthenticatedRoleName", {
+      value: authenticatedRoles.adminRole.roleName,
+      exportName: app.logicalPrefixedName("CognitoAdminAuthRole"),
+    });
+  }
+}
